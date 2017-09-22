fd3b03df29cde54cba6be6ffaf1f54bee05aeeac
wzhifang <wangzhifang@51talk.com>
2017-09-22 16:44:53 +0800

    Initial Commit

---
+++ coredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata
+//
+//  Created by wzhifang on 2017/9/22.
+//  Copyright © 2017年 wzhifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coredata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coredata
+//
+//  Created by wzhifang on 2017/9/22.
+//  Copyright © 2017年 wzhifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
